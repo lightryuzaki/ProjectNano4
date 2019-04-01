be0aced992c1c0f37d510148659be0d6645cb3b4
Jay Friestad <packfanjay@hotmail.com>
2019-04-01 18:56:45 -0500

    HP Washing Removed
    Characters now gain 1.5x regular HP to bypass HP washing

--- src/client/MapleCharacter.java
+++ src/client/MapleCharacter.java
@@ -1079,18 +1079,18 @@ public class MapleCharacter extends AbstractMapleCharacterObject {
         int addhp = 0, addmp = 0;
         int job_ = job.getId() % 1000; // lame temp "fix"
         if (job_ == 100) {                      // 1st warrior
-            addhp += Randomizer.rand(200, 250);
+            addhp += Randomizer.rand(300, 325);
         } else if (job_ == 200) {               // 1st mage
             addmp += Randomizer.rand(100, 150);
         } else if (job_ % 100 == 0) {           // 1st others
-            addhp += Randomizer.rand(100, 150);
+            addhp += Randomizer.rand(150, 225);
             addhp += Randomizer.rand(25, 50);
         } else if (job_ > 0 && job_ < 200) {    // 2nd~4th warrior
-            addhp += Randomizer.rand(300, 350);
+            addhp += Randomizer.rand(450, 525);
         } else if (job_ < 300) {                // 2nd~4th mage
             addmp += Randomizer.rand(450, 500);
         } else if (job_ > 0) {                  // 2nd~4th others
-            addhp += Randomizer.rand(300, 350);
+            addhp += Randomizer.rand(450, 500);
             addmp += Randomizer.rand(150, 200);
         }
         
