b0d81b53290605d701416fd441d5ff3e96064724
JR <minimucho1@gmail.com>
2019-04-01 22:43:19 -0500

    Issue #6 | JR | Fix party EXP calculation

--- src/server/life/MapleMonster.java
+++ src/server/life/MapleMonster.java
@@ -528,7 +528,7 @@ public class MapleMonster extends AbstractLoadedMapleLife {
             if (mc.getId() == killerId) {
                 calculatedExp = totalPartyExp * killerPercent;
             } else {
-                calculatedExp = (totalPartyExp * leecherPercent) / numExpSharersFloat;
+                calculatedExp = (totalPartyExp * leecherPercent) / (float)(numExpSharers - 1);
             }
             partyExpReward.put(mc, calculatedExp);
         }
