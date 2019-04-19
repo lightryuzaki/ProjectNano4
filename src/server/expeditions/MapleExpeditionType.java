cc6775525ef242e6c1ef8b8d6777d11dc7680f68
JR <minimucho1@gmail.com>
2019-04-19 12:43:03 -0500

    Issue #66 | JR | Changed all expeditions to a minimum party size of 3

--- src/server/expeditions/MapleExpeditionType.java
+++ src/server/expeditions/MapleExpeditionType.java
@@ -32,14 +32,14 @@ import constants.ServerConstants;
 public enum MapleExpeditionType {
 
     BALROG_EASY(3, 30, 50, 255, 5),
-    BALROG_NORMAL(6, 30, 50, 255, 5),
-    SCARGA(6, 30, 100, 255, 5),
+    BALROG_NORMAL(3, 30, 50, 255, 5),
+    SCARGA(3, 30, 100, 255, 5),
     SHOWA(3, 30, 100, 255, 5),
-    ZAKUM(6, 30, 50, 255, 5),
-    HORNTAIL(6, 30, 100, 255, 5),
-    CHAOS_ZAKUM(6, 30, 120, 255, 5),
-    CHAOS_HORNTAIL(6, 30, 120, 255, 5),
-    PINKBEAN(6, 30, 120, 255, 5),
+    ZAKUM(3, 30, 50, 255, 5),
+    HORNTAIL(3, 30, 100, 255, 5),
+    CHAOS_ZAKUM(3, 30, 120, 255, 5),
+    CHAOS_HORNTAIL(3, 30, 120, 255, 5),
+    PINKBEAN(3, 30, 120, 255, 5),
     CWKPQ(6, 30, 90, 255, 5);   // CWKPQ min-level 90, found thanks to Cato
     
     private int minSize;
